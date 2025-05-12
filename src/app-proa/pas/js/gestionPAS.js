4ace3c1366952ee20f6b614c09f776ad7c497605
ALANGMupv <alanguevara36@gmail.com>
2025-05-12 23:18:12 +0200

    feat: full stack Gestión PAS

--- src/app-proa/pas/js/gestionPAS.js
+++ src/app-proa/pas/js/gestionPAS.js
@@ -8,6 +8,40 @@ function toggleOpciones(icono) {
     });
 }
 
+// --- Cargar asignaturas desde el JSON externo y mostrarlas ---
+fetch('/src/api/data/asignaturas.json')
+    .then(response => response.json())
+    .then(asignaturas => {
+        const tabla = document.querySelector('.tabla-asignaturas');
+
+        asignaturas.forEach(asig => {
+            const fila = document.createElement('div');
+            fila.classList.add('fila');
+
+            // --- Código HTML filas asignatuaras ---
+            fila.innerHTML = `
+        <span data-label="Código">${asig.codigo}</span>
+        <span data-label="Nombre">${asig.nombre}</span>
+        <span data-label="Departamento">${asig.departamento}</span>
+        <span data-label="Créditos">${asig.creditos}</span>
+        <button class="btn" data-label="Ficha">Ver detalles</button>
+        <div class="menu-opciones-wrapper" data-label="Asignar">
+          <img src="../icons/menu.svg" alt="Opciones" class="icono-opciones" onclick="toggleOpciones(this)" />
+          <div class="menu-desplegable">
+            <button onclick="alert('Asignar alumno a ${asig.nombre}')">Asignar alumno</button>
+            <button onclick="alert('Asignar profesor a ${asig.nombre}')">Asignar profesor</button>
+          </div>
+        </div>
+      `;
+
+            tabla.appendChild(fila);
+        });
+    })
+    .catch(error => {
+        console.error('Error al cargar las asignaturas:', error);
+    });
+
+
 // Cerrar menú si se hace clic fuera
 document.addEventListener('click', function (e) {
     if (!e.target.closest('.menu-opciones-wrapper')) {
