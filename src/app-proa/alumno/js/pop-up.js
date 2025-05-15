3b3d19c129d8bec0f033d26502b10367e9aa8291
ALANGMupv <alanguevara36@gmail.com>
2025-05-15 23:55:39 +0200

    chore: lógica popup dentro de hacer examen alumno

--- src/app-proa/alumno/js/pop-up.js
+++ src/app-proa/alumno/js/pop-up.js
@@ -18,3 +18,31 @@ botonAceptar.addEventListener('click', function () {
 botonCancelar.addEventListener('click', function () {
     popup.style.display = 'none'; // Oculta el pop-up
 });
+
+
+// Referencias al pop-up cancelar
+const popupSalir = document.getElementById('popup-volver-cancelar');
+const btnSalirSi = document.getElementById('popup-volver-cancelar-si');
+const btnSalirNo = document.getElementById('popup-volver-cancelar-no');
+
+// Evento para botón "Cancelar"
+document.querySelector('button[type="reset"]').addEventListener('click', function(e) {
+    e.preventDefault();
+    popupSalir.style.display = 'flex';
+});
+
+// Evento para enlace "Volver"
+document.getElementById('volver').addEventListener('click', function(e) {
+    e.preventDefault();
+    popupSalir.style.display = 'flex';
+});
+
+// Acción si el usuario confirma salir
+btnSalirSi.addEventListener('click', function() {
+    window.location.href = 'examenes-alumno.html';
+});
+
+// Cierra el pop-up si el usuario cancela
+btnSalirNo.addEventListener('click', function() {
+    popupSalir.style.display = 'none';
+});
